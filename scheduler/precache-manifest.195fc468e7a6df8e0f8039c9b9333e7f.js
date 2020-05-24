c57548dd03c5f6fa1f8f2f5c05065afd9e24ab1a
Faisal Ansari <faisal6795@gmail.com>
2020-05-24 16:35:05 +0530

    Add task scheduler

---
+++ scheduler/precache-manifest.195fc468e7a6df8e0f8039c9b9333e7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1ca230f67fdb2cee8ef90b3d60edce5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "839f5ed44059a3612cff",
+    "url": "/static/css/2.c6231af7.chunk.css"
+  },
+  {
+    "revision": "533e50f1552264bbcc77",
+    "url": "/static/css/main.82cc635f.chunk.css"
+  },
+  {
+    "revision": "839f5ed44059a3612cff",
+    "url": "/static/js/2.525f1b30.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.525f1b30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "533e50f1552264bbcc77",
+    "url": "/static/js/main.fe213cd0.chunk.js"
+  },
+  {
+    "revision": "af34574360f2e8c45495",
+    "url": "/static/js/runtime-main.916fb653.js"
+  }
+]);
