9e7c3699c11a13b24c309675a75d901a99e0dc6b
Faisal Ansari <faisal.ansari@iifl.com>
2021-02-20 12:51:22 +0530

    Updated relative paths

--- malabis/precache-manifest.1cd51eb2507f506d498064ea852b6a26.js
+++ malabis/precache-manifest.1cd51eb2507f506d498064ea852b6a26.js
@@ -1,214 +1,214 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "c3f08b5019c24e6eac3edd7f10babcf0",
-    "url": "/index.html"
+    "url": "./index.html"
   },
   {
     "revision": "3e95288a17d2683a9275",
-    "url": "/static/css/2.03fe9875.chunk.css"
+    "url": "./static/css/2.03fe9875.chunk.css"
   },
   {
     "revision": "96cba71690e8d559df9b",
-    "url": "/static/css/main.d2e72ca3.chunk.css"
+    "url": "./static/css/main.d2e72ca3.chunk.css"
   },
   {
     "revision": "3e95288a17d2683a9275",
-    "url": "/static/js/2.1cb713a3.chunk.js"
+    "url": "./static/js/2.1cb713a3.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/static/js/2.1cb713a3.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.1cb713a3.chunk.js.LICENSE.txt"
   },
   {
     "revision": "96cba71690e8d559df9b",
-    "url": "/static/js/main.ab1b0034.chunk.js"
+    "url": "./static/js/main.ab1b0034.chunk.js"
   },
   {
     "revision": "5100b11ae128284e9080",
-    "url": "/static/js/runtime-main.88fbabb7.js"
+    "url": "./static/js/runtime-main.88fbabb7.js"
   },
   {
     "revision": "d182a4e3cece6a15375867f6efea9d9e",
-    "url": "/static/media/Mulish-Bold.d182a4e3.ttf"
+    "url": "./static/media/Mulish-Bold.d182a4e3.ttf"
   },
   {
     "revision": "e128ac44faa84b2d59c10c016fad0778",
-    "url": "/static/media/Mulish-Regular.e128ac44.ttf"
+    "url": "./static/media/Mulish-Regular.e128ac44.ttf"
   },
   {
     "revision": "0eba93ff4b706e42927f1b733daa4212",
-    "url": "/static/media/banner.0eba93ff.jpg"
+    "url": "./static/media/banner.0eba93ff.jpg"
   },
   {
     "revision": "79d14d8ce8e9782fba992de1383cfe5e",
-    "url": "/static/media/blend-silk-1.79d14d8c.jpg"
+    "url": "./static/media/blend-silk-1.79d14d8c.jpg"
   },
   {
     "revision": "0c3550ac743d0429ceb378b67d674c96",
-    "url": "/static/media/blend-silk-2.0c3550ac.jpg"
+    "url": "./static/media/blend-silk-2.0c3550ac.jpg"
   },
   {
     "revision": "b61444f698b9302986ae08f80a77fe47",
-    "url": "/static/media/blue-bana-1.b61444f6.jpg"
+    "url": "./static/media/blue-bana-1.b61444f6.jpg"
   },
   {
     "revision": "45bbb33b3097ce3f55e25b55ca1ed9b9",
-    "url": "/static/media/blue-bana-2.45bbb33b.jpg"
+    "url": "./static/media/blue-bana-2.45bbb33b.jpg"
   },
   {
     "revision": "b57e4b266a30fe188194696f22b2b9eb",
-    "url": "/static/media/blue-tanchui-1.b57e4b26.jpg"
+    "url": "./static/media/blue-tanchui-1.b57e4b26.jpg"
   },
   {
     "revision": "ade828aa83d27c19a677a42fab8b715c",
-    "url": "/static/media/blue-tanchui-2.ade828aa.jpg"
+    "url": "./static/media/blue-tanchui-2.ade828aa.jpg"
   },
   {
     "revision": "666b4657b7207bc8b1da2a003d9c7a22",
-    "url": "/static/media/blue-zari-1.666b4657.jpg"
+    "url": "./static/media/blue-zari-1.666b4657.jpg"
   },
   {
     "revision": "a70a0a739fe0a083b3c4d45724be375d",
-    "url": "/static/media/blue-zari-2.a70a0a73.jpg"
+    "url": "./static/media/blue-zari-2.a70a0a73.jpg"
   },
   {
     "revision": "57f2b3ffe56702980956dbc4a03283fa",
-    "url": "/static/media/cart.57f2b3ff.svg"
+    "url": "./static/media/cart.57f2b3ff.svg"
   },
   {
     "revision": "3f121cd572428d96dbc9e8bbd58bfdcc",
-    "url": "/static/media/chiku-kanjee-1.3f121cd5.jpg"
+    "url": "./static/media/chiku-kanjee-1.3f121cd5.jpg"
   },
   {
     "revision": "afd1210f6424ddc461128cd7cdf7b0aa",
-    "url": "/static/media/chiku-kanjee-2.afd1210f.jpg"
+    "url": "./static/media/chiku-kanjee-2.afd1210f.jpg"
   },
   {
     "revision": "8edc43216de7bb13e98dc0109fc3fc17",
-    "url": "/static/media/cross.8edc4321.svg"
+    "url": "./static/media/cross.8edc4321.svg"
   },
   {
     "revision": "5c2d1de1514f0752deb16dbcb519e6f2",
-    "url": "/static/media/empty_cart.5c2d1de1.svg"
+    "url": "./static/media/empty_cart.5c2d1de1.svg"
   },
   {
     "revision": "24dc18d7ddc3a89d5da77db80da5b5ff",
-    "url": "/static/media/empty_fav.24dc18d7.svg"
+    "url": "./static/media/empty_fav.24dc18d7.svg"
   },
   {
     "revision": "de66f0417490f23a1c7e8265024aff59",
-    "url": "/static/media/gold-kanjee-1.de66f041.jpg"
+    "url": "./static/media/gold-kanjee-1.de66f041.jpg"
   },
   {
     "revision": "719908e6ccb0ffdc1270b0eaf4554137",
-    "url": "/static/media/gold-kanjee-2.719908e6.jpg"
+    "url": "./static/media/gold-kanjee-2.719908e6.jpg"
   },
   {
     "revision": "24520ac31f5f97afbfa90589b3508832",
-    "url": "/static/media/gold-tissu-1.24520ac3.jpg"
+    "url": "./static/media/gold-tissu-1.24520ac3.jpg"
   },
   {
     "revision": "ead28ffbec6dd6ce2b04b8cd86253d32",
-    "url": "/static/media/gold-tissu-2.ead28ffb.jpg"
+    "url": "./static/media/gold-tissu-2.ead28ffb.jpg"
   },
   {
     "revision": "309eac4b57642ce5a4e431f0be1e79e2",
-    "url": "/static/media/heart.309eac4b.svg"
+    "url": "./static/media/heart.309eac4b.svg"
   },
   {
     "revision": "b45924ca32cf01c6c9aeebb50f2a22d9",
-    "url": "/static/media/heart_fill.b45924ca.svg"
+    "url": "./static/media/heart_fill.b45924ca.svg"
   },
   {
     "revision": "601d23ca313f7828ecbea7556ebab9e6",
-    "url": "/static/media/hindi.601d23ca.svg"
+    "url": "./static/media/hindi.601d23ca.svg"
   },
   {
     "revision": "9b1e418b72a9c460e1403a5ee2a0fbdd",
-    "url": "/static/media/kota-resham-1.9b1e418b.jpg"
+    "url": "./static/media/kota-resham-1.9b1e418b.jpg"
   },
   {
     "revision": "ee079cf2395159b44d1d5f959bf35e2b",
-    "url": "/static/media/kota-resham-2.ee079cf2.jpg"
+    "url": "./static/media/kota-resham-2.ee079cf2.jpg"
   },
   {
     "revision": "9743e0653d8b0b5081661fffa384220f",
-    "url": "/static/media/kota-zari-1.9743e065.jpg"
+    "url": "./static/media/kota-zari-1.9743e065.jpg"
   },
   {
     "revision": "ccbb0881efa46714ac8f3e6d3ca7202f",
-    "url": "/static/media/kota-zari-2.ccbb0881.jpg"
+    "url": "./static/media/kota-zari-2.ccbb0881.jpg"
   },
   {
     "revision": "85524d6af0671b3ba2da6426626d3d05",
-    "url": "/static/media/logo.85524d6a.svg"
+    "url": "./static/media/logo.85524d6a.svg"
   },
   {
     "revision": "a388781f3266b2e15a3f7963c6b0fa93",
-    "url": "/static/media/minus.a388781f.svg"
+    "url": "./static/media/minus.a388781f.svg"
   },
   {
     "revision": "8da3f613838e2ede1ab3c4df4c369be3",
-    "url": "/static/media/no_results.8da3f613.svg"
+    "url": "./static/media/no_results.8da3f613.svg"
   },
   {
     "revision": "19ac04fa42a61a0c488d806733a11c11",
-    "url": "/static/media/phone.19ac04fa.svg"
+    "url": "./static/media/phone.19ac04fa.svg"
   },
   {
     "revision": "2619b6e1b449d64c6b2aeac97ccf83ed",
-    "url": "/static/media/plus.2619b6e1.svg"
+    "url": "./static/media/plus.2619b6e1.svg"
   },
   {
     "revision": "44232c0963448c8b9b1b1e748ef7ea8c",
-    "url": "/static/media/red-tanchui-1.44232c09.jpg"
+    "url": "./static/media/red-tanchui-1.44232c09.jpg"
   },
   {
     "revision": "133da7188f30418f4dd2bf2a3913349c",
-    "url": "/static/media/red-tanchui-2.133da718.jpg"
+    "url": "./static/media/red-tanchui-2.133da718.jpg"
   },
   {
     "revision": "76563ffd036fa1a293596cb273c9a940",
-    "url": "/static/media/search.76563ffd.svg"
+    "url": "./static/media/search.76563ffd.svg"
   },
   {
     "revision": "d1a962741b5e3251514abd34eefe5dc2",
-    "url": "/static/media/silk-kanjee-1.d1a96274.jpg"
+    "url": "./static/media/silk-kanjee-1.d1a96274.jpg"
   },
   {
     "revision": "82edad4231d919512ce33c22621b7ac6",
-    "url": "/static/media/silk-kanjee-2.82edad42.jpg"
+    "url": "./static/media/silk-kanjee-2.82edad42.jpg"
   },
   {
     "revision": "b263901267170346800947b0a369ecc2",
-    "url": "/static/media/silky-bana-1.b2639012.jpg"
+    "url": "./static/media/silky-bana-1.b2639012.jpg"
   },
   {
     "revision": "689f4b03d53713eda9c13e20b8609f7c",
-    "url": "/static/media/silky-bana-2.689f4b03.jpg"
+    "url": "./static/media/silky-bana-2.689f4b03.jpg"
   },
   {
     "revision": "10d24b651c65d09ce0ade73b2bc5a091",
-    "url": "/static/media/success.10d24b65.svg"
+    "url": "./static/media/success.10d24b65.svg"
   },
   {
     "revision": "9ad70d96f6bf4f3112b2c4a864faddc2",
-    "url": "/static/media/whatsapp.9ad70d96.svg"
+    "url": "./static/media/whatsapp.9ad70d96.svg"
   },
   {
     "revision": "8e32248c0038312e81aeff00dff167fc",
-    "url": "/static/media/white-zari-1.8e32248c.jpg"
+    "url": "./static/media/white-zari-1.8e32248c.jpg"
   },
   {
     "revision": "43b63b7efa52f62e2c462a583fa1da1c",
-    "url": "/static/media/white-zari-2.43b63b7e.jpg"
+    "url": "./static/media/white-zari-2.43b63b7e.jpg"
   },
   {
     "revision": "7bf5782a9d3d41790fc438d39000d5ec",
-    "url": "/static/media/yellow-tanchui-1.7bf5782a.jpg"
+    "url": "./static/media/yellow-tanchui-1.7bf5782a.jpg"
   },
   {
     "revision": "1f677679beceadbf8e6446989bc1263e",
-    "url": "/static/media/yellow-tanchui-2.1f677679.jpg"
+    "url": "./static/media/yellow-tanchui-2.1f677679.jpg"
   }
 ]);
