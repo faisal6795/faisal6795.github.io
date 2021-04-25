95d8f66dcc9e96ef31ce3fc9ec55953d819a6f80
Faisal Ansari <faisal6795@gmail.com>
2021-04-25 17:42:51 +0530

    Added todolist

---
+++ todolist/precache-manifest.59840adb406a8dbd8685ebd909e440f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "878d2125f4cf9e6e72b888bb95d02e95",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "3b06dc2e11375d1e3183",
+    "url": "/todolist/static/css/main.b0a8a265.chunk.css"
+  },
+  {
+    "revision": "beb805ec4c48ba0c6059",
+    "url": "/todolist/static/js/2.82844ea8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todolist/static/js/2.82844ea8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b06dc2e11375d1e3183",
+    "url": "/todolist/static/js/main.f767d0c5.chunk.js"
+  },
+  {
+    "revision": "658823e3cb77b75eedbc",
+    "url": "/todolist/static/js/runtime-main.587ab855.js"
+  },
+  {
+    "revision": "d45dda2b245e849168ec9809a67da174",
+    "url": "/todolist/static/media/logo.d45dda2b.svg"
+  }
+]);
